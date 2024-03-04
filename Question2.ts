4aa7bbb73877d8239c17d76abd378501da2cfabe
Ambreen Rais <159732503+Ambreen16@users.noreply.github.com>
2024-03-04 23:53:35 +0500

    Create Question2.ts

---
+++ Question2.ts
@@ -0,0 +1,6 @@
+/*Q2
+Personal Message: Store a person’s name in a variable, and print a message to that person.
+Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+ */
+let friend_name: string="Maha";
+console.log(`"Hello ${friend_name},Would you like to join us for next trip?"`);
